43f5129f2e8b933d6603d23404b339ded9959a50
roberto14118927 <robertoruiz141189@gmail.com>
2019-03-04 11:23:17 -0600

    4

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import router from "./routes/router";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 // bodyParser
 server.app.use(bodyParser.urlencoded({ extended: true }));
